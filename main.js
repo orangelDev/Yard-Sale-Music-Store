adafce71b45008e64f1e9eb3bc7096e8e06e0324
Orangel Brito <orangeleando@gmail.com>
2022-08-25 20:28:45 -0500

    Se modifico el main.js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu')
 const menuCartIcon = document.querySelector('.navbar-shopping-cart')
+const productDetailCloseicon = document.querySelector('.product-detail-close')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+const productDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCartIcon.addEventListener('click', toggleCartAside);
+productDetailCloseicon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -27,7 +30,10 @@ function toggleMobileMenu() {
         shoppingCartContainer.classList.add('inactive');
     }
 
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle('inactive');
+
 }
 
 function toggleCartAside() {
@@ -36,7 +42,22 @@ function toggleCartAside() {
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
+ 
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+    if (!isProductDetailClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
     shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -59,12 +80,15 @@ productList.push({
 
 // Hacemos una iteracion por cada producto de nuestro arrays de productos, y por cada uno estamos creando - maquetando la estructura html que debemos insertarle a nuestro documento html
 
-for (product of productList) {
+function renderProducts(arr) {
+
+    for (product of arr) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -76,8 +100,7 @@ for (product of productList) {
     const productName = document.createElement('p');
     productName.innerText = product.name;
 
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
+    productInfoDiv.append(productPrice, productName);
 
     const productInfoFigure = document.createElement('figure');
     const productImgCart = document.createElement('img'); 
@@ -85,13 +108,13 @@ for (product of productList) {
 
     productInfoFigure.appendChild(productImgCart);
 
-    productInfo.appendChild(productInfoDiv); 
-    productInfo.appendChild(productInfoFigure);
+    productInfo.append(productInfoDiv, productInfoFigure); 
 
-    productCard.appendChild(productImg);
-    productCard.appendChild(productInfo);
+    productCard.append(productImg, productInfo);
 
     cardsContainer.appendChild(productCard);
 
 
+  }
 }
+renderProducts(productList);
